14ca744f482ae7eb5909e3b34e1d15b8800606d0
hongyuan <1500482815@qq.com>
2017-06-02 17:34:59 +0800

    wifi to mobile net

--- util_lib/src/main/java/com/github/wrongyuan/util_lib/network/NetChangeBroadCastReciver.java
+++ util_lib/src/main/java/com/github/wrongyuan/util_lib/network/NetChangeBroadCastReciver.java
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
+import android.os.Handler;
+import android.os.Looper;
 
 public class NetChangeBroadCastReciver extends BroadcastReceiver {
 
@@ -12,6 +14,8 @@ public class NetChangeBroadCastReciver extends BroadcastReceiver {
     public NetChangeBroadCastReciver() {
         super();
     }
+
+    private Handler handler;
 
     public interface OnNetChangedListener {
         void onNetChanged(NetworkStatus currNetStatus);
@@ -22,11 +26,28 @@ public class NetChangeBroadCastReciver extends BroadcastReceiver {
     }
 
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public void onReceive(final Context context, Intent intent) {
         if (ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())) {
-            if (listener != null) {
-                listener.onNetChanged(NetUtil.getNetWorkState(context));
+            //wifi由开而关，先不通知，5000ms后检测到依然无网络，此时通知。原因是:移动网络打开时，BroadCastReciver会接到2次消息，第一次检测网络是不通的，其实只是在切换中，第二次检测网络为移动网络了。
+            if (intent.getIntExtra(ConnectivityManager.EXTRA_NETWORK_TYPE, -1) == ConnectivityManager.TYPE_WIFI && intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false)) {
+                if (handler == null)
+                    handler = new Handler(Looper.getMainLooper());
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        NetworkStatus networkStatus = NetUtil.getNetWorkState(context);
+                        if (networkStatus == NetworkStatus.NETWORK_NONE)
+                            if (listener != null) {
+                                listener.onNetChanged(networkStatus);
+                            }
+                    }
+                }, 5000);
+            } else {
+                if (listener != null) {
+                    listener.onNetChanged(NetUtil.getNetWorkState(context));
+                }
             }
         }
     }
+
 }
